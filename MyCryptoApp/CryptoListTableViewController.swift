6ecaeafcad6db6bc2d4167c21ef038908de9d014
Kahina Lounis <kahina.lounis@europcar.com>
2021-05-21 17:40:58 +0200

    make some changes

--- MyCryptoApp/CryptoListTableViewController.swift
+++ MyCryptoApp/CryptoListTableViewController.swift
@@ -12,10 +12,8 @@ import Alamofire
 
 class CryptoListTableViewController: UITableViewController {
    
-    @IBOutlet weak var cryptoNameLabel: UILabel!
     let networkService = NetworkService()
-
-    var items: [DisplayableCoinInfo] = []
+    var crypto = [Info]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +21,12 @@ class CryptoListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+       // return items.count
+          return 10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = items[indexPath.row]
-        cryptoNameLabel?.text = item.fullNameLabelText
         return cell
         
     }
@@ -40,19 +37,5 @@ class CryptoListTableViewController: UITableViewController {
  
 }
   
-extension CryptoListTableViewController {
-    func fetchCrypto() {
-        AF.request("https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=EUR")
-            .validate()
-            .responseDecodable(of: Info.self) { (response) in
-                guard let info = response.value else { return }
-                // show as many cells as there are crypto
-                self.items = info.Data
-                self.tableView.reloadData()
-            }
 
-    }
-    
-    
-}
 
--- MyCryptoApp/Networking/NetworkService .swift
+++ MyCryptoApp/Networking/NetworkService .swift
@@ -10,11 +10,13 @@ import Alamofire
 
 
 class NetworkService {
+    
     func fetchCrypto() {
         AF.request("https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=EUR")
             .validate()
             .responseDecodable(of: Info.self) { (response) in
                 guard let info = response.value else { return }
+                //for testing
                 print(info.Data[0].CoinInfo.Name)
                 print(info.Data[0].CoinInfo.FullName)
                 print(info.Data[1].CoinInfo.Name)
@@ -22,9 +24,6 @@ class NetworkService {
                 print(info.Data[0].DISPLAY.EUR.PRICE)
                 print(info.Data[1].CoinInfo.FullName)
                 print(info.Data[1].CoinInfo.ImageUrl)
-
-    
-  }
-
-}
+            }
+    }
 }
